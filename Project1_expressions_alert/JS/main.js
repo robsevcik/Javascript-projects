e627eaababc016f22158f7839c71088498139e9e
robsevcik <robert.a.sevcik@gmail.com>
2021-04-13 14:47:02 -0700

    Update main.js

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,1 +1,12 @@
-window.alert ("Hello, World");
+
+window.alert("hello, world");
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3*3);
+
+var A = "this is half";
+var B = "of the sentence";
+document.write (A + B);
