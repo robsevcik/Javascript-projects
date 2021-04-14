a7e3835b2fad7a2ba3dbe3ddab6b7423e77581e9
robsevcik <robert.a.sevcik@gmail.com>
2021-04-13 17:38:11 -0700

    Create main3.js

---
+++ Project3_math_operators/JS/main3.js
@@ -0,0 +1,13 @@
+
+  document.write("test");
+
+
+function addition_Function()  { 
+    var addition = 2+2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
