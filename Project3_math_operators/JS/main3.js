30a7be12f4f6f45ad91c4b0c55af56e1a3a413d3
robsevcik <robert.a.sevcik@gmail.com>
2021-04-14 11:47:47 -0700

    Update main3.js

--- Project3_math_operators/JS/main3.js
+++ Project3_math_operators/JS/main3.js
@@ -1,13 +1,55 @@
+window.alert(Math.random());//random number generator//
 
-  document.write("test");
+  document.write("test");//to test my conncetion to html//
 
 
-function addition_Function()  { 
+function addition_Function()  { //math operators//
     var addition = 2+2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
 
 function subtraction_Function() {
     var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+    document.getElementById("Math1").innerHTML = "5 - 2 = " + Subtraction;
 }
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math4").innerHTML = "6 x 8 = " + simple_Math;
+
+}
+
+
+function division() {
+    var simple_Math1 = 48 / 6;
+    document.getElementById("Math3").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math2").innerHTML = "when you divide 25 by 6 you have a remainder of: " + simple_Math2;
+}
+
+function negation_Operator()  {
+    var x = 10;
+    document.getElementById("Math5").innerHTML = -x;   
+}
+
+
+
+var X = 5;
+X++;
+document.write(x);
+
+
+
+
+var X = 5.25;
+X--;
+document.write(x);
+
+
+window.alert(Math.random());// random number generators again
+
+window.alert(Math.random() * 100)
+
