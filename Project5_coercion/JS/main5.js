4d1f0e2674ec7249620e376eebd2ba590383f99a
robsevcik <robert.a.sevcik@gmail.com>
2021-04-15 07:27:14 -0700

    Create main5.js

---
+++ Project5_coercion/JS/main5.js
@@ -0,0 +1,22 @@
+document.write(typeof "Word");
+
+document.write(typeof 3);
+
+
+document.write("10" + 5);
+
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+console.log(10 < 2);
