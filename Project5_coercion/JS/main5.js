4fb38e1a5519596dee11b1df74a2c3be595e7867
robsevcik <robert.a.sevcik@gmail.com>
2021-04-15 07:49:59 -0700

    Update main5.js

--- Project5_coercion/JS/main5.js
+++ Project5_coercion/JS/main5.js
@@ -10,7 +10,7 @@ function my_Function() {
     document.getElementById("Test").innerHTML = 0/0;
 }
 
-
+//examples of operators//
 document.write(2E310);
 
 document.write(-3E310);
@@ -20,3 +20,20 @@ document.write(10 > 2);
 document.write(10 < 2);
 
 console.log(10 < 2);
+
+document.write(5 > 2 && 10 > 4);// and//
+
+document.write(5 > 10 || 10 > 4);//or//
+
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+X = 82;
+y = "82";
+document.write(X === Y);
+
+
+document.write(3 == 11);
+//end examples//
