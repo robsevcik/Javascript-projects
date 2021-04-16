8fb856f87496d5ec9fbfd416b32286bb4d5d2b81
robsevcik <robert.a.sevcik@gmail.com>
2021-04-16 09:37:02 -0700

    Update main6.js

--- project6_ternary_operators_constructors/JS/main6.js
+++ project6_ternary_operators_constructors/JS/main6.js
@@ -6,7 +6,7 @@ function Vote_Function()  {
 
 }
 
-
+//ternary//
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
@@ -15,9 +15,33 @@ function Vehicle(Make, Model, Year, Color) {
 }
 var Jack = new Vehicle("Dodge","Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Eric = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML = 
     "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
     " manufactured in " + Erik.Vehicle_Year;
+}
+//using new and this//
+function firstfunction() {
+    document.getElementById("New_and_This").innerHTML = (20 > 10);
+}
+
+//constructor function//
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+var myFather = new Person("John", "Doe", 50, "blue");
+
+//nested function//
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
 }
