841c33ed44134453d0a29c8a9085f8c5c261f40a
robsevcik <robert.a.sevcik@gmail.com>
2021-04-15 17:35:40 -0700

    Update main6.js

--- project6_ternary_operators_constructors/JS/main6.js
+++ project6_ternary_operators_constructors/JS/main6.js
@@ -1,6 +1,23 @@
 function Vote_Function()  {
-    var age, Can vote;
-    age = document.getElementById("age").value:
-    Can_vote = (age < 18) ? "you are not old enough to vote":"You can vote!";
-    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+    var age, Can_vote;
+    age = document.getElementById("age").value;
+    Can_vote = (age < 18) ? "you are not old enough":"You are old enough";
+    document.getElementById("vote").innerHTML = Can_vote + " to vote.";
+
+}
+
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge","Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Eric = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = 
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
 }
