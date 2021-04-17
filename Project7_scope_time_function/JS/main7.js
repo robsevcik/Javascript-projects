50050c1641359181923c70fa4e4dbaff7b6b72c7
robsevcik <robert.a.sevcik@gmail.com>
2021-04-17 10:32:06 -0700

    Update main7.js

--- Project7_scope_time_function/JS/main7.js
+++ Project7_scope_time_function/JS/main7.js
@@ -3,7 +3,7 @@ var X = 10;
 function Add_numbers_1() {
     document.write(20 + X + "<br>");
 }
-function Add-numbers_2() {
+function Add_numbers_2() {
     document.write(X + 100);
 }
 Add_numbers_1();
@@ -47,7 +47,7 @@ function Age_Function() {
     else {
         Vote = "You are not old enough to vote!";
     }
-    document.getElementById("How_old_are_you?").innHTML = Vote;
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
 //else statement//
 function Time_function() {
@@ -63,4 +63,13 @@ function Time_function() {
         Reply = "It is evening time.";
     }
     document.getElementById("Time_of_day").innerHTML = Reply;
+}
+
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
