55d5afc073dc09776ca8b4ab78d889192178c71b
robsevcik <robert.a.sevcik@gmail.com>
2021-04-18 11:21:44 -0700

    Create async.js

---
+++ Project9_countdown_slideshow/JS/async.js
@@ -0,0 +1,14 @@
+alert(document.getElementById("p1").innerHTML);
+
+
+
+function count_To_Ten();  {
+    var Digit = "";
+    var X = 1;
+    while (X < 11)  {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+    console.log(result);
+]
