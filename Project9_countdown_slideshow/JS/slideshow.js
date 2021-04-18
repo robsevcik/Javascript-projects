fa97dc5235784bb32000b0ac4f6261f71683ab12
robsevcik <robert.a.sevcik@gmail.com>
2021-04-17 17:00:30 -0700

    Update slideshow.js

--- Project9_countdown_slideshow/JS/slideshow.js
+++ Project9_countdown_slideshow/JS/slideshow.js
@@ -1,5 +1,4 @@
 
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -27,19 +26,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
