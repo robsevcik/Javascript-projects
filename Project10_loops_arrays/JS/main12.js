67dd3011f82621ac2e331ada3ec60f384bf7b87a
robsevcik <robert.a.sevcik@gmail.com>
2021-04-22 16:54:05 -0700

    Update main12.js

--- Project10_loops_arrays/JS/main12.js
+++ Project10_loops_arrays/JS/main12.js
@@ -7,23 +7,23 @@ function Hello_World_Function() {
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-// Create gradient
+// red gradient//
 var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
 grd.addColorStop(0, "red");
 grd.addColorStop(1, "white");
 
-// Fill with gradient
+
 ctx.fillStyle = grd;
 ctx.fillRect(10, 10, 150, 80);
 
 
-//Gradient//
-var c = document.getElementById("Gradient");
-var ctx = c.getContext("2d");
+//black Gradient//
+var d = document.getElementById("Gradient");
+var ct = c.getContext("2d");
 
 var grd = ctx.createLinearGradient(0, 0, 170, 0);
 grd.addColorStop(0, "black");
 grd.addColorStop(1, "white");
 
-ctx.fillStyle = grd;
-ctx.fillRect(20, 20, 150, 100);
+ct.fillStyle = grd;
+ct.fillRect(20, 20, 150, 100);
