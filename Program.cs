f9825771767ac16733ab257815720260990a9ccc
robsevcik <robert.a.sevcik@gmail.com>
2021-05-12 18:05:02 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The tech Academy");
+        Console.WriteLine("student Daly Report");
+        Console.WriteLine("what is your name?");
+        Console.WriteLine("what course are you on?");
+        Console.WriteLine(" what page number?");
+        Console.WriteLine("do you need help with anything? Please answer true or false");
+        Console.WriteLine("Were there any positive experiences you'd like to share?Please give specifics");
+        Console.WriteLine("is there any other feed back you'd like to provide? Please be specific");
+        Console.WriteLine("how many hours did you study today?");
+        Console.WriteLine("Thank you for your answere. An instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
