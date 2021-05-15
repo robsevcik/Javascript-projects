10ebb8271d8fad8e738af8c1b251fc334d59e661
robsevcik <robert.a.sevcik@gmail.com>
2021-05-15 11:47:00 -0700

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -3,33 +3,33 @@
 
     class Program
     {
-        static void Main()
+    static void Main()
     {
-        Console.WriteLine("The Tech Academy");
-
-        Console.WriteLine("Student Daily Report");
-
-        Console.WriteLine("what is your name?");
-            string name = Console.ReadLine();
-
-        Console.WriteLine("What course are you on?");
-        int course = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("What page number?");
-        int page = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Do you need help with anything? True or False.");
-        bool help = Convert.ToBoolean(Console.ReadLine());
-
-        Console.WriteLine("Were there any possitive experiences you'd like to share? Please give specifics");
-        string experiences = Console.ReadLine();
-
-        Console.WriteLine("How many hours did you study?");
-        int hours = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
-
+        Console.WriteLine("Enter a Number to multiply by 50");
+        int usernumber = Console.ReadLine();
+        Console.WriteLine(usernumber * 50);
         Console.ReadLine();
+
+        Console.WriteLine("Enter a number to add 25 to");
+        int usernumber1 = Console.ReadLine();
+        Console.WriteLine(usernumber1 + 25);
+        Console.ReadLine();
+
+        Console.WriteLine("Enter a number to be divided by 12.5");
+        int username2 = Console.ReadLine();
+        Console.WriteLine(usernumber2 / 12.5);
+        Console.ReadLine();
+
+
+        Console.WriteLine("enter a number");
+        int username3 = Console.ReadLine();
+        if username3 is => 50 Console.WriteLine(true)
+        Console.ReadLine();
+
+        Console.WriteLine("Enter a number")
+        int username4 = Console.ReadLine();
+        int remainder = username4 / 7;
+        Console.WriteLine(remainder);
     }
 }
 
