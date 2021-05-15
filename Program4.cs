dbfd2c84a1203b10329881091156648080bbf7b3
robsevcik <robert.a.sevcik@gmail.com>
2021-05-15 14:23:21 -0700

    Create Program4.cs

---
+++ Program4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dui_questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your age?");
+         int age =  Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?"):
+           bool DUI = Convert.ToBool(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            license = age < 15 && DUI = false && < 3;
+
+            Console.WriteLine(license);
+        }
+    }
+}
