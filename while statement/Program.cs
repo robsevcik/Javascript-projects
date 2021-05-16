273b4d1bb11b6cfd83a44c15c77cadd52c69456b
robsevcik <robert.a.sevcik@gmail.com>
2021-05-16 15:04:18 -0700

    Create Program.cs

---
+++ while statement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace while_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+
+                    default:
+                        Console.WriteLine("you are wrong");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 62:
+                        Console.WriteLine("you guessed 62, try again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 5:
+                        Console.WriteLine("you guessed the number 5! Correct!");
+                        isGuessed = true;
+                        break;
+                }
+
+            }
+
+            while (!isGuessed);
+
+            string v = Console.ReadLine(); ;
+            }
+        }
+    }
+
