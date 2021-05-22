8e9b1bc0b055874b5806618bb5ab92d4d46157b4
robsevcik <robert.a.sevcik@gmail.com>
2021-05-22 14:43:48 -0700

    Create int1.cs

---
+++ Project7_scope_time_function/Iteration assignment/int1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Iteration_assignment
+{
+    internal class int1
+    {
+        public static implicit operator int1(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
