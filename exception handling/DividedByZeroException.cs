b29d79ba23848ae3373b2f279b6a31619bf94c50
robsevcik <robert.a.sevcik@gmail.com>
2021-05-23 13:11:32 -0700

    Create DividedByZeroException.cs

---
+++ exception handling/DividedByZeroException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace exception_handling
+{
+    [Serializable]
+    internal class DividedByZeroException : Exception
+    {
+        public DividedByZeroException()
+        {
+        }
+
+        public DividedByZeroException(string message) : base(message)
+        {
+        }
+
+        public DividedByZeroException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DividedByZeroException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
