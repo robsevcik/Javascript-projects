bc48b046099f72ee49f07dc8e47b60a814d84345
robsevcik <robert.a.sevcik@gmail.com>
2021-05-24 07:53:08 -0700

    Update Program.cs

--- exception handling/Program.cs
+++ exception handling/Program.cs
@@ -8,14 +8,25 @@ namespace exception_handling
         {
             try
             {
-                Console.WriteLine("Pick a number");
+                list<string> intlist = new list<string>();
+                intlist.Add(4);
+                intlist.Add(3);
+                intlist.Add(7);
+               
+
+                Console.WriteLine("Pick a number do dived each number by");
 
                 int numberOne = Convert.ToInt32(Console.ReadLine());
+
                 Console.WriteLine("pick a second numbner");
                 int numberTwo = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("dividing the two..");
                 int numberthree = numberOne / numberTwo;
                 Console.WriteLine(numberOne + "  divided by" + numberTwo + "  equals" + numberthree);
+                int numberthree = numberTwo / numberTwo;
+                Console.WriteLine(numberOne + "  divided by" + numberTwo + "  equals" + numberFour);
+                int numberthree = numberTwo / numberThree;
+                Console.WriteLine(numberOne + "  divided by" + numberTwo + "  equals" + numberFive);
                 Console.ReadLine();
             }
             catch (FormatException ex)
