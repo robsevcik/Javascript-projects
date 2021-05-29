f6d4c004aeb429ee256ab4dcc809d52ee500bfe4
robsevcik <robert.a.sevcik@gmail.com>
2021-05-29 10:57:46 -0700

    Create Card.cs

---
+++ 21/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    public class Card
+    {
+
+        public Card()
+        {
+
+            
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
