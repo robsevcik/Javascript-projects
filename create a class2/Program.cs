d0fd637cf728312f3e62a91312fb362d246e484c
robsevcik <robert.a.sevcik@gmail.com>
2021-06-02 12:49:10 -0700

    Create Program.cs

---
+++ create a class2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace create_a_class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            static void MyMethod1();
+            static void MyMethod2()
+            static void MyMethod3();
+
+        }
+    }
+}
