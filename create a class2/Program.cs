97dfef3e578622c18ce7e7374afc19c67c62cabe
robsevcik <robert.a.sevcik@gmail.com>
2021-06-08 16:57:05 -0700

    Update Program.cs

--- create a class2/Program.cs
+++ create a class2/Program.cs
@@ -8,9 +8,35 @@ namespace create_a_class2
         {
             Math math = new Math();
 
-            math.MyMethod1();
-            math.MyMethod2();
-            math.MyMethod3();
+            Console.WriteLine("enter a number to multiply by");
+            int numone = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter another");
+            int numtwo = Convert.ToInt32(Console.ReadLine());
+
+
+            math.MyMethod1(numone, numtwo);
+
+
+            Console.WriteLine("enter a number to divide by");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter another");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+            math.MyMethod2(a, b);
+
+
+            Console.WriteLine("enter a number to subtract");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter another");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            math.MyMethod3(c,d);
 
         }
     }
