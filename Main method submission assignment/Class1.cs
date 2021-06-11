06b915cfe9779583224a65261f6a007d5db94c5d
robsevcik <robert.a.sevcik@gmail.com>
2021-06-11 11:28:09 -0700

    Create Class1.cs

---
+++ Main method submission assignment/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_method_submission_assignment
+{
+    class Math
+    {
+        
+public int MyMethod1(int numtwo)
+        {
+
+            int i = numtwo * 5;
+
+            return i;
+        }
+
+        public double MyMethod1(int numtwo)
+        {
+
+            double i = numtwo + 5;
+
+            return i;
+        }
+
+        public string MyMethod1(int numtwo)
+        {
+
+            int i = numtwo - 5;
+
+            return i;
+        }
+
+
+    }
