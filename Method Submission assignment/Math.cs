62bb93a957ab394083385e3ad9fdd03ecd65c3f8
robsevcik <robert.a.sevcik@gmail.com>
2021-06-12 13:35:48 -0700

    Create Math.cs

---
+++ Method Submission assignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_assignment
+{
+    class Math
+    {
+        public int MyMethod1(int numone, int numtwo);
+        {
+            
+            int i =  numone * numtwo * 5;
+
+            return i;
+        }
+
+
+
+    }
+}
