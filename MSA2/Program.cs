a5e61ee4d409ed1cd02af2069eb7320928022ad2
robsevcik <robert.a.sevcik@gmail.com>
2021-06-12 14:47:25 -0700

    Create Program.cs

---
+++ MSA2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Method_Submission_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math math = new();
+
+            Console.WriteLine("enter a number to multiply by");
+            int numone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number, or zero");
+            int numtwo = Convert.ToInt32(Console.ReadLine());
+
+
+            int d = math.MyMethod1(numone);
+            Console.WriteLine("here is your number, times 5 = "+ d);
+
+            Console.WriteLine("the other number was: "+ numtwo);
+
+        }
+    }
+}
