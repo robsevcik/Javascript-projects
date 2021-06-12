2371585b28c1a5cae9240450cd0e142257df76e3
robsevcik <robert.a.sevcik@gmail.com>
2021-06-12 14:47:34 -0700

    Create Math.cs

---
+++ MSA2/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_assignment
+{
+    class Math
+    {
+        public int MyMethod1(int numone)
+        {
+            
+            int i =  numone * 5;
+            
+
+            return i;
+
+           
+
+
+        }
+
+
+
+    }
+}
