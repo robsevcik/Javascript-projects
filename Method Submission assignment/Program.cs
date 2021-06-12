59b464aa0849def40c0d36d71bad12b3473d7525
robsevcik <robert.a.sevcik@gmail.com>
2021-06-12 13:35:11 -0700

    Create Program.cs

---
+++ Method Submission assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Method_Submission_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math math = new();
+
+            Console.WriteLine("enter a number to multiply by");
+            int numone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number, or nothing");
+            int numtwo = Convert.ToInt32(Console.ReadLine());
+
+
+            int d = math.MyMethod1(numone, numtwo);
+            Console.WriteLine("total of numbers multiplyed"+ d);
+
+        }
+    }
+}
