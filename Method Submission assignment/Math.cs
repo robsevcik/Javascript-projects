02947408cab07ef47497457de865a9ec30e6ad13
robsevcik <robert.a.sevcik@gmail.com>
2021-06-12 17:39:24 -0700

    Update Math.cs

--- Method Submission assignment/Math.cs
+++ Method Submission assignment/Math.cs
@@ -10,13 +10,24 @@ namespace Method_Submission_assignment
     {
         public int MyMethod1(int numone, int numtwo)
         {
-            
-            int i =  numone * numtwo * 5;
+
+            int i = numone * numtwo * 5;
 
             return i;
         }
 
+        int x = Console.ReadLine();
+        string yStr = Console.ReadLine();
 
-
+        if (string = "")
+        {
+          a =  x + 5;
+          Console.WriteLine(a);
+        }
+        else
+        {
+        int y = Convert.ToInt32(yStr);
+        b = y + 5;
+        Console.writeLine(b);
     }
 }
