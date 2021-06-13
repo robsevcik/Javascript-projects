3fe78c2e42b9a35e79679102d2e749ceef5e3b41
robsevcik <robert.a.sevcik@gmail.com>
2021-06-13 12:41:05 -0700

    Update Program.cs

--- Method Submission assignment/Program.cs
+++ Method Submission assignment/Program.cs
@@ -4,6 +4,8 @@ namespace Method_Submission_assignment
 {
     class Program
     {
+        private static int numtwo;
+
         static void Main()
         {
             Math math = new();
@@ -11,14 +13,16 @@ namespace Method_Submission_assignment
             Console.WriteLine("enter a number to multiply by");
             int numone = Convert.ToInt32(Console.ReadLine());
 
+            int d = math.MyMethod(numone);
+            Console.WriteLine("total first number multiplyed, times 5 = " + d);
+
             Console.WriteLine("enter a second number, or it will be zero");
            string stringNumTwo = Console.ReadLine();
 
             if (stringNumTwo == "")
             {
 
-                int d = math.MyMethod(numone);
-                Console.WriteLine("total first number multiplyed, times 5 = " + d);
+                Console.WriteLine("You must enter a number please.");
             }
             else
             {
