6b68a0f04c1b40ca8db833ac1748b5d25d4dbe22
robsevcik <robert.a.sevcik@gmail.com>
2021-06-13 12:08:00 -0700

    Update Program.cs

--- Method Submission assignment/Program.cs
+++ Method Submission assignment/Program.cs
@@ -11,13 +11,20 @@ namespace Method_Submission_assignment
             Console.WriteLine("enter a number to multiply by");
             int numone = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("enter a second number, or zero");
-            int numtwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a second number, or it will be zero");
+           string stringNumTwo = Console.ReadLine();
 
+            if (stringNumTwo == "")
+            {
 
-            int d = math.MyMethod1(numone, numtwo);
-            Console.WriteLine("total of numbers multiplyed, times 5 = "+ d);
-
+                int d = math.MyMethod(numone);
+                Console.WriteLine("total first number multiplyed, times 5 = " + d);
+            }
+            else
+            {
+                int c = math.MyMethod1(numone, numtwo);
+                Console.WriteLine("total of both numbers multiplyed, times 5 = " + c);
+            }
         }
     }
 }
