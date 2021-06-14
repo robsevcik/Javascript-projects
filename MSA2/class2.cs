59144809284110ec0dd66c48b5a626d2cfde1a54
robsevcik <robert.a.sevcik@gmail.com>
2021-06-14 16:11:10 -0700

    Create class2.cs

---
+++ MSA2/class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Submission_assignment
+{
+    class class2
+    {
+        public void  MyMethod(int numone, int numtwo)
+        {
+            
+            int i =  numone * 5;
+
+            Console.WriteLine(numtwo);
+            
+
+            return ;
+
+           
+
+
+        }
+
+
+
+    }
+}
