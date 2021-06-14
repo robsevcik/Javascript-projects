76c9edf9b83857ddfc96612156f0fb427c53ba01
robsevcik <robert.a.sevcik@gmail.com>
2021-06-14 15:37:42 -0700

    Update Math.cs

--- MSA2/Math.cs
+++ MSA2/Math.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Method_Submission_assignment
+namespace Method_Class_Submission_assignment
 {
     class Math
     {
-        public static void  MyMethod(int numone)
+        public void  MyMethod(int numone)
         {
             
             int i =  numone * 5;
             
 
-            return;
+            return ;
 
            
 
